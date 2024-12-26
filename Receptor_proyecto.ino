bb7589c306823211007bf8c409dca5a03d9450d2
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2024-12-26 16:46:37 -0600

    Update Receptor_proyecto.ino

--- Receptor_proyecto.ino
+++ Receptor_proyecto.ino
@@ -1,22 +1,28 @@
 //----------------------------Proyecto Instrumentacion Electrica (Receptor)---
-
 #include <RH_ASK.h>		// incluye libreria RadioHead.h
 #include <SPI.h> 		// incluye libreria SPI necesaria por RadioHead.h
-
-RH_ASK rf_driver;		// crea objeto para modulacion por ASK
-
+RH_ASK rf_driver(4000);		// crea objeto para modulacion por ASK
 const int AZUL = 2;
 const int ROJO = 3;
 const int AMARILLO = 4;
 const int VERDE = 5;
+const int MOTOR_AA = 6; //MOTOR A
+const int MOTOR_AB = 7;
+const int MOTOR_BA = 8; //MOTOR B
+const int MOTOR_BB = 9;
  
 void setup(){
+    Serial.begin(9600);
     pinMode(AZUL, OUTPUT);
     pinMode(ROJO, OUTPUT);
     pinMode(AMARILLO, OUTPUT);
     pinMode(VERDE, OUTPUT);
+    pinMode(MOTOR_AA, OUTPUT);
+    pinMode(MOTOR_AB, OUTPUT);
+    pinMode(MOTOR_BA, OUTPUT);
+    pinMode(MOTOR_BB, OUTPUT);
     rf_driver.init();		// inicializa objeto con valores por defecto
-}
+  }
  
 void loop(){
     uint8_t buf[1];			// espacio para mensaje recibido de 1 caracter
@@ -25,29 +31,46 @@ void loop(){
     if (rf_driver.recv(buf, &buflen))	{
       if((char)buf[0]== '1' ){
             digitalWrite(AZUL, HIGH);
-            delayMicroseconds(1000); 
+            digitalWrite(MOTOR_AA, HIGH);
+            digitalWrite(MOTOR_AB, LOW);
+            digitalWrite(MOTOR_BA, HIGH);
+            digitalWrite(MOTOR_BB, LOW);
+            delay(1000); 
         }						 
         else if((char)buf[0]=='2'){
             digitalWrite(ROJO, HIGH);
-            delayMicroseconds(1000);   
+            digitalWrite(MOTOR_AA, LOW);
+            digitalWrite(MOTOR_AB, HIGH);
+            digitalWrite(MOTOR_BA, LOW);
+            digitalWrite(MOTOR_BB, HIGH);
+            delay(1000);   
         }       
         
         else if((char)buf[0]=='3'){
             digitalWrite(AMARILLO, HIGH);
-            delayMicroseconds(1000);   
+            digitalWrite(MOTOR_AA, HIGH);
+            digitalWrite(MOTOR_AB, LOW);
+            digitalWrite(MOTOR_BA, LOW);
+            digitalWrite(MOTOR_BB, HIGH);
+            delay(1000);   
         }        
        
         else if((char)buf[0]=='4'){
             digitalWrite(VERDE, HIGH);
-            delayMicroseconds(1000);     
+            digitalWrite(MOTOR_AA, LOW);
+            digitalWrite(MOTOR_AB, HIGH);
+            digitalWrite(MOTOR_BA, HIGH);
+            digitalWrite(MOTOR_BB, LOW);
+            delay(1000);     
         }  
     }
-
+  digitalWrite(MOTOR_AA, LOW);  
+  digitalWrite(MOTOR_AB, LOW);
+  digitalWrite(MOTOR_BA, LOW);
+  digitalWrite(MOTOR_BB, LOW);
   digitalWrite(AZUL, LOW);
   digitalWrite(ROJO, LOW);
   digitalWrite(AMARILLO, LOW);
   digitalWrite(VERDE, LOW);
   delay(10);  
-
-
 }
